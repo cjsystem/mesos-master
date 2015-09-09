f9dafb841d4e91feae5cab0cca0811d93b2b1c15
周　贇皓@SYS <shu-inkou@dmm.com>
2015-09-09 15:17:37 +0900

    run +x

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,13 @@
 
 PRINCIPAL=${PRINCIPAL:-root}
 
-
+if [ -n "$SECRET" ]; then
+    export MESOS_AUTHENTICATE=true
+    export MESOS_AUTHENTICATE_SLAVES=true
+    touch /tmp/credentials
+    chmod 600 /tmp/credentials
+    echo -n "$PRINCIPAL $SECRET" > /tmp/credentials
+    export MESOS_CREDENTIALS=/tmp/credentials
+fi
 
 exec "$@"
